9da5d3e80ab18f3ad35ba687cfeb7c255dd3fd90
Zhiyuan Zeng <created_equal@foxmail.com>
2022-10-13 23:55:21 +0800

    modularized trap

--- os8/os8_trap.h
+++ os8/os8_trap.h
@@ -1,9 +1,20 @@
 #ifndef OS8_TRAP_H
 #define OS8_TRAP_H
 
-#include "../utils/types.h"
+#include "../utils/defs.h"
+
+void os8_set_usertrap();
+void os8_set_kerneltrap();
+
+struct trapframe* os8_get_trapframe();
+uint64 os8_get_kernel_sp();
+
+void os8_call_userret();
+void os8_finish_usertrap(int cause);
+void os8_error_in_trap(int status);
+
+void os8_super_external_handler();
 
 void trap_init();
-void usertrapret();
 
 #endif // OS8_TRAP_H
