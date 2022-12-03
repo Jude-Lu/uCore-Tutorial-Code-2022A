35be26584b6608f11565135182645e2e701eb525
Zhiyuan Zeng <created_equal@foxmail.com>
2022-12-03 23:19:49 +0800

    Add comment on signal module

--- signal/signal.c
+++ signal/signal.c
@@ -12,7 +12,7 @@ int sigaction(uint32 signum, uint64 va_act, uint64 va_oldact)
 {
 	if (signum < 1 || signum > 31)
 		return -1;
-	if (signum == SIGKILL || signum == SIGSTOP || signum == SIGCONT)
+	if (signum == SIGKILL || signum == SIGSTOP || signum == SIGCONT) // 这三种信号必须由内核处理
 		return -1;
 	struct sigaction *action = (sig_context->get_curr_sig_block)()->sig_actions + signum;
 	if (va_oldact && sig_context->copyout(sig_context->get_curr_pagetable(), va_oldact, (char*)action, sizeof(struct sigaction)) < 0)
@@ -43,29 +43,33 @@ int sigkill(int pid, uint32 signum)
 {
 	if (signum < 1U || signum > 31U)
 		return -1;
+	
 	struct signal_block *sig_block = (sig_context->pid2sig_block)(pid);
 	if (sig_block == NULL || sig_block->killed)
 		return -1;
+	
+	// 如果该进程正在处理某个信号，那么屏蔽的信号就是它对应sigaction数据结构的sa_mask；反之就是进程signal_block数据结构的signal_mask
 	uint32 mask;
 	if (sig_block->handling_sig)
 		mask = sig_block->sig_actions[sig_block->handling_sig].sa_mask;
 	else
 		mask = sig_block->signal_mask;
+	
 	if (mask & (1U << signum))
 		return -1;
 	else
 	{
 		if (signum == SIGKILL)
 		{
-			sig_block->killed = 1;
-			sig_block->frozen = 0;
+			sig_block->killed = 1; // 目前的设计是，等到调度到对应进程的某个线程时杀死该进程，而不是立即处理
+			sig_block->frozen = 0; // 认为这个进程一定没有被冻住，否则会调度不到该进程
 		}
 		else if (signum == SIGCONT)
 			sig_block->frozen = 0;
 		else if (signum == SIGSTOP)
 			sig_block->frozen = 1;
 		else
-			sig_block->signals |= (1U << signum);
+			sig_block->signals |= (1U << signum); // 在signal里面标记好需要处理signum这个信号，交由用户注册的signal handler处理
 		return 0;
 	}
 }
--- signal/signal.h
+++ signal/signal.h
@@ -5,26 +5,27 @@
 #include "signal_dependency.h"
 
 struct sigaction {
-	void (*handler)(int);
-	uint64   sa_mask;
+	void (*handler)(int); ///< 该信号（由用户注册的）signal handler
+	uint64   sa_mask; // 在处理该信号的时候，需要屏蔽的信号
 };
 
 struct signal_block {
 	uint32 signals; // 要响应的信号
 	uint32 signal_mask; // 要屏蔽的信号
 	uint32 handling_sig; // 正在处理的信号
-	int killed, frozen;
-	struct sigaction sig_actions[32];
+	int killed, frozen; // 当前进程是否已经被杀死/是否被冻住
+	struct sigaction sig_actions[32]; // 每个种类的信号对应的sigaction数据结构
 };
 
 struct signal_context {
 	pagetable_t (*get_curr_pagetable)();  ///< 得到当前进程的页表
 
-	struct signal_block* (*get_curr_sig_block)(); //< 得到当前进程的signal_block
-	struct signal_block* (*pid2sig_block)(int pid);
+	struct signal_block* (*get_curr_sig_block)(); ///< 得到当前进程的signal_block
+	struct signal_block* (*pid2sig_block)(int pid); ///< 返回pid进程对应的signal_block数据结构的指针。向该进程发送信号的时候需要对该数据结构进行操作
 	
-	void (*customized_sigreturn)();
+	void (*customized_sigreturn)(); ///< 恢复到执行signal handler之前的状态
 
+	/// sigaction需要和进程虚拟内存里面的数据进行交互
 	int (*copyin)(pagetable_t pagetable, char* dst, uint64 srcva, uint64 len);
 	int (*copyout)(pagetable_t pagetable, uint64 dstva, char* src, uint64 len);
 };
